59fc70748240e5395d89b8610ce6bc1be51d6654
Loop54 Publisher <github@loop54.com>
2021-11-18 09:37:19 +0000

    Release v1.2.1

--- lib/Client.php
+++ lib/Client.php
@@ -8,7 +8,7 @@ class Client
     use OpenAPIWrapper;
 
     const APIVERSION = 'V3';
-    const LIBVERSION = 'php:V3:1.2.0';
+    const LIBVERSION = 'php:V3:1.2.1';
     private $apikey;
     private $remoteClientInfo;
     private $httpClient;
