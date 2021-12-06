56fca0a4eff29cd0bba0333ae5f3b85830c818f7
Lukas Nordin <lukas@loop54.com>
2021-12-06 17:26:45 +0100

    Bump version (lib/client.php)

--- lib/Client.php
+++ lib/Client.php
@@ -8,7 +8,7 @@ class Client
     use OpenAPIWrapper;
 
     const APIVERSION = 'V3';
-    const LIBVERSION = 'php:V3:1.3.0';
+    const LIBVERSION = 'php:V3:1.3.1';
     private $apikey;
     private $remoteClientInfo;
     private $httpClient;
