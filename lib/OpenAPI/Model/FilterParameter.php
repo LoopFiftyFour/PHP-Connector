fc39f58133c044a5810ccb98b0d63070666b725c
Lukas Nordin <lukas@loop54.com>
2023-03-08 11:06:01 +0100

    qebF9OHl: Fix deprecated ArrayAccess types on newer PHP versions

--- lib/OpenAPI/Model/FilterParameter.php
+++ lib/OpenAPI/Model/FilterParameter.php
@@ -210,7 +210,7 @@ class FilterParameter implements ModelInterface, ArrayAccess
      *
      * @return boolean
      */
-    public function offsetExists($offset)
+    public function offsetExists($offset): bool
     {
         return isset($this->container[$offset]);
     }
@@ -222,7 +222,7 @@ class FilterParameter implements ModelInterface, ArrayAccess
      *
      * @return mixed
      */
-    public function offsetGet($offset)
+    public function offsetGet($offset): mixed
     {
         return isset($this->container[$offset]) ? $this->container[$offset] : null;
     }
@@ -235,7 +235,7 @@ class FilterParameter implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         if (is_null($offset)) {
             $this->container[] = $value;
@@ -251,7 +251,7 @@ class FilterParameter implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetUnset($offset)
+    public function offsetUnset($offset): void
     {
         unset($this->container[$offset]);
     }
