7b2efefae4f9f8b6662004b21df95c89d2a9545b
Lukas Nordin <lukas@loop54.com>
2023-03-08 11:24:14 +0100

    qebF9OHl: Fix deprecated ArrayAccess types on newer PHP versions (#29)

--- lib/OpenAPI/Model/DistinctFacetItemSortingParameter.php
+++ lib/OpenAPI/Model/DistinctFacetItemSortingParameter.php
@@ -331,7 +331,7 @@ class DistinctFacetItemSortingParameter implements ModelInterface, ArrayAccess
      *
      * @return boolean
      */
-    public function offsetExists($offset)
+    public function offsetExists($offset): bool
     {
         return isset($this->container[$offset]);
     }
@@ -343,7 +343,7 @@ class DistinctFacetItemSortingParameter implements ModelInterface, ArrayAccess
      *
      * @return mixed
      */
-    public function offsetGet($offset)
+    public function offsetGet($offset): mixed
     {
         return isset($this->container[$offset]) ? $this->container[$offset] : null;
     }
@@ -356,7 +356,7 @@ class DistinctFacetItemSortingParameter implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         if (is_null($offset)) {
             $this->container[] = $value;
@@ -372,7 +372,7 @@ class DistinctFacetItemSortingParameter implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetUnset($offset)
+    public function offsetUnset($offset): void
     {
         unset($this->container[$offset]);
     }
--- lib/OpenAPI/Model/GetRecommendedEntitiesRequest.php
+++ lib/OpenAPI/Model/GetRecommendedEntitiesRequest.php
@@ -265,7 +265,7 @@ class GetRecommendedEntitiesRequest implements ModelInterface, ArrayAccess
      *
      * @return boolean
      */
-    public function offsetExists($offset)
+    public function offsetExists($offset): bool
     {
         return isset($this->container[$offset]);
     }
@@ -277,7 +277,7 @@ class GetRecommendedEntitiesRequest implements ModelInterface, ArrayAccess
      *
      * @return mixed
      */
-    public function offsetGet($offset)
+    public function offsetGet($offset): mixed
     {
         return isset($this->container[$offset]) ? $this->container[$offset] : null;
     }
@@ -290,7 +290,7 @@ class GetRecommendedEntitiesRequest implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         if (is_null($offset)) {
             $this->container[] = $value;
@@ -306,7 +306,7 @@ class GetRecommendedEntitiesRequest implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetUnset($offset)
+    public function offsetUnset($offset): void
     {
         unset($this->container[$offset]);
     }
