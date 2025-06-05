8f3b1f960e9426827da1fcafca548c09cc43d402
Loop54 Publisher <github@loop54.com>
2025-06-05 12:27:53 +0000

    Release v3.0.5

--- lib/Client.php
+++ lib/Client.php
@@ -8,7 +8,7 @@ class Client
     use OpenAPIWrapper;
 
     const APIVERSION = 'V3';
-    const LIBVERSION = 'php:V3:3.0.4';
+    const LIBVERSION = 'php:V3:3.0.5';
     private $apikey;
     private $remoteClientInfo;
     private $httpClient;
